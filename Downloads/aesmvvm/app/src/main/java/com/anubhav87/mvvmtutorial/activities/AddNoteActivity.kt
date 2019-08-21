c8bea41ede21ce05eac7e733efaf0de051b4771d
Hein Thura Wynnn <heinthurawynn.developer@gmail.com>
2019-08-21 15:51:10 +0630

    enc dec is added

--- Downloads/aesmvvm/app/src/main/java/com/anubhav87/mvvmtutorial/activities/AddNoteActivity.kt
+++ Downloads/aesmvvm/app/src/main/java/com/anubhav87/mvvmtutorial/activities/AddNoteActivity.kt
@@ -1,16 +1,32 @@
 package com.anubhav87.mvvmtutorial.activities
 
 import android.app.Activity
+import android.app.AlertDialog
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Base64
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.EditText
 import android.widget.Toast
 import com.anubhav87.mvvmtutorial.R
+import com.anubhav87.mvvmtutorial.SharedPreference
 import kotlinx.android.synthetic.main.activity_add_note.*
+import java.security.MessageDigest
+import javax.crypto.Cipher
+import javax.crypto.spec.SecretKeySpec
 
 class AddNoteActivity : AppCompatActivity() {
+    var id=0
+    val AES = "AES"
+    var encTitle: String = ""
+    var encDesc: String = ""
+    var decTitle: String = ""
+    var decDesc: String = ""
+    var enc_dec: Boolean = false
+
     companion object {
         const val EXTRA_TITLE = "com.anubhav87.mvvmtutorial.EXTRA_TITLE"
         const val EXTRA_DESCRIPTION = "com.anubhav87.mvvmtutorial.EXTRA_DESCRIPTION"
@@ -51,4 +67,91 @@ class AddNoteActivity : AppCompatActivity() {
         setResult(Activity.RESULT_OK, data)
         finish()
     }
+    fun btnEnc(view: View){
+        val sharedPreference = SharedPreference(this)
+//        val password = sharedPreference.getValueString("code")
+        val password = "1234"
+        encDesc  = encrypt(edit_text_description.text.toString(),password.toString())
+        edit_text_description.setText(encDesc)
+        enc_dec = true
+        Toast.makeText(this,"Encrypted",Toast.LENGTH_LONG).show()
+    }
+    fun btnDec(view: View){
+
+        val sharedPreference = SharedPreference(this)
+//        val password = sharedPreference.getValueString("code")
+        val password = "1234"
+        decDesc  = decrypt(edit_text_description.text.toString(),password.toString())
+        edit_text_description.setText(decDesc)
+        enc_dec = false
+        Toast.makeText(this,"Decrypted",Toast.LENGTH_LONG).show()
+
+    }
+
+    @Throws(Exception::class)
+    private fun decrypt(outputString: String, password: String): String {
+        val key = generateKey(password)
+        val c = Cipher.getInstance(AES)
+        c.init(Cipher.DECRYPT_MODE, key)
+        val decodedValue = Base64.decode(outputString, Base64.DEFAULT)
+        val decValue = c.doFinal(decodedValue)
+        return String(decValue)
+    }
+
+    @Throws(Exception::class)
+    private fun encrypt(Data: String, password: String): String {
+        val key = generateKey(password)
+        val c = Cipher.getInstance(AES)
+        c.init(Cipher.ENCRYPT_MODE, key)
+        val encVal = c.doFinal(Data.toByteArray())
+        return Base64.encodeToString(encVal, Base64.DEFAULT)
+    }
+
+    @Throws(Exception::class)
+    private fun generateKey(password: String): SecretKeySpec {
+        val digest = MessageDigest.getInstance("SHA-256")
+        val bytes = password.toByteArray(charset("UTF-8"))
+        digest.update(bytes, 0, bytes.size)
+        val key = digest.digest()
+        return SecretKeySpec(key, "AES")
+    }
+
+    fun showCreateCategoryDialog() {
+        val context = this
+        val builder = AlertDialog.Builder(context)
+        builder.setTitle("New Category")
+
+        // https://stackoverflow.com/questions/10695103/creating-custom-alertdialog-what-is-the-root-view
+        // Seems ok to inflate view with null rootView
+        val view = layoutInflater.inflate(R.layout.activity_add_note, null)
+
+        val categoryEditText = view.findViewById(R.id.categoryEditText) as EditText
+
+        builder.setView(view);
+
+        // set up the ok button
+        builder.setPositiveButton(android.R.string.ok) { dialog, p1 ->
+            val newCategory = categoryEditText.text
+            var isValid = true
+            if (newCategory.isBlank()) {
+                categoryEditText.error = getString(R.string.validation_empty)
+                isValid = false
+            }
+
+            if (isValid) {
+                // do something
+            }
+
+            if (isValid) {
+                dialog.dismiss()
+            }
+        }
+
+        builder.setNegativeButton(android.R.string.cancel) { dialog, p1 ->
+            dialog.cancel()
+        }
+
+        builder.show();
+    }
 }
+
